3b501363650c9bcdc7b0cbdbeb114bab21b6bab0
hcoullon <helene.coullon@inria.fr>
2022-07-18 18:02:34 +0200

    base

---
+++ movie/movie.py
@@ -0,0 +1,41 @@
+from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
+from ariadne.constants import PLAYGROUND_HTML
+from flask import Flask, request, jsonify
+
+import resolvers as r
+
+PORT = 3001
+HOST = '0.0.0.0'
+app = Flask(__name__)
+
+### ADD THINGS HERE
+#
+###
+
+# root message
+@app.route("/", methods=['GET'])
+def home():
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+
+#####
+# graphql entry points
+
+@app.route('/graphql', methods=['GET'])
+def playground():
+    return PLAYGROUND_HTML, 200
+    
+@app.route('/graphql', methods=['POST'])
+def graphql_server():
+    data = request.get_json()
+    success, result = graphql_sync(
+    schema,
+    data,
+    context_value=None,
+    debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+if __name__ == "__main__":
+    print("Server running in port %s"%(PORT))
+    app.run(host=HOST, port=PORT)
